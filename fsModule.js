ec63db8269cd3a4d8240647f0b924e2672f9a8a5
KOKAB NAVEED <kokabnaveed2002@gmail.com>
2025-09-15 14:20:36 +0500

    Learn FS Module

--- fsModule.js
+++ fsModule.js
@@ -1,7 +1,24 @@
-person = {
-    name:'Kokab',
-    age:23,
-    isEmployeed:false
-}
+// read file
 
-module.exports = person;
+const fs = require('fs');
+
+// fs.readFile('ReadFile.txt', 'utf8' , (err,data)=>{
+//     if(err){
+//         console.error(`Error Reading File ${err}`);
+//         return;
+//     }
+//     console.log(`Data is ${data}`);
+// })
+
+
+// Write File
+
+const content = "Hello, I am learning Node Js";
+
+fs.writeFile('output.txt', content, (err) => {
+    if (err) {
+        console.error("Error Writing To file: ", err);
+        return;
+    }
+    console.log('Written Successfully to the File')
+})
