e27cd26da41e96f2071ea5a15aaff19b59d2668c
KOKAB NAVEED <kokabnaveed2002@gmail.com>
2025-09-15 15:43:48 +0500

    learn Lodash

--- index.js
+++ index.js
@@ -10,3 +10,11 @@ subResult = math.subtract(5,3);
 
 console.log(`Addition Result is ${addResult}`)
 console.log(`Subtraction Result is ${subResult}`)
+
+const lodash = require('lodash')
+
+
+const numbers = [1,2,3,4,5]
+const reversed = lodash.reverse(numbers)
+
+console.log(reversed);
